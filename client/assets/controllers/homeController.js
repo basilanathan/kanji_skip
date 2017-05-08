0e4a54e28e2ff448827ebdd32cd3becd84f3f497
Urian Chang <urianchang@gmail.com>
2017-05-07 23:08:50 -0700

    Added introductory text to SKIP system and worked on CSS for landing page

--- client/assets/controllers/homeController.js
+++ client/assets/controllers/homeController.js
@@ -1,9 +1,4 @@
 //: Controller for Home page
 myApp.controller('homeController', ['$scope', 'kanjiFactory', function ($scope, kanjiFactory) {
-    //: This is to check if kanji character is rendered on HTML -- it does
-    $scope.kanji = {};
-    
-    kanjiFactory.showRandom(function(data) {
-    	$scope.kanji = data;
-    });
+
 }]);
