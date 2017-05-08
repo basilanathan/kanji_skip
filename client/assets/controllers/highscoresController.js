733cbef0a2f94fe078b1abd0c8621de01826b248
Urian Chang <urianchang@gmail.com>
2017-05-08 00:31:48 -0700

    Added reference to kanji dictionary in About section

--- client/assets/controllers/highscoresController.js
+++ client/assets/controllers/highscoresController.js
@@ -1,9 +1,6 @@
 myApp.controller('highscoresController', ['$scope', '$route', 'highscoreFactory', function ($scope, $route, highscoreFactory) {
-    //: This is to check if kanji character is rendered on HTML -- it does
-
     $scope.scores = [];
     highscoreFactory.showScores(function(data) {
         $scope.scores = data;
     });
-
 }]);
